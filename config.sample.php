fdd57a94f30bc32d5c27b78338bea380e90e84d0
BibleForge <info@bibleforge.com>
2009-08-30 12:46:41 -0500

    Adding .gitignore.  Removing config.php; adding config.sample.php.

---
+++ config.sample.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Bible Forge (alpha testing)
+ *
+ * @date    08-30-09
+ * @version 0.1 alpha 2
+ * @link http://www.BibleForge.com
+ */
+
+/**
+ * Configure the constants to match your enviroment,
+ * and then rename this file to config.php.
+ */
+
+/// MySQL 
+define('DB_SERVER', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'bf');
+
+/// Sphinx
+define('SPHINX_SERVER', '127.0.0.1');
+define('SPHINX_PORT', 3312);
