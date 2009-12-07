4c8c2d815c1804005e6cb0210ee7e849cc6d8589
BibleForge <info@bibleforge.com>
2009-12-06 20:10:16 -0600

    More work done on morphological searching.
    Should be a working version of morhplogy.php.

--- functions/morphology.php
+++ functions/morphology.php
@@ -8,133 +8,106 @@
  * @link http://www.BibleForge.com
  */
 
-/// Non-grammatical
-define('ADDED', 1);
-define('DIVINE', 2);
-define('RED', 3);
-
-/// Parts of speech
-define('NOUN', 4);
-define('VERB', 5);
-define('ADJECTIVE', 6);
-define('ADVERB', 7);
-define('DEFINITE_ARTICLE', 8);
-define('PRONOUN', 9);
-define('PERSONAL_PRONOUN', 10);
-define('RELATIVE_PRONOUN', 11);
-define('RECIPROCAL_PRONOUN', 12);
-define('DEMONSTRATIVE_PRONOUN', 13);
-define('CORRELATIVE_PRONOUN', 14);
-define('INTERROGATIVE_PRONOUN', 15);
-define('INDEFINITE_PRONOUN', 16);
-///FIXME: #12 CORRELATIVE PRONOUN OR INTERROGATIVE PRONOUN needs to be analyze.
-define('REFLEXIVE_PRONOUN', 17);
-define('POSESSIVE_PRONOUN', 18);
-define('CONJUNCTION', 19);
-define('CONDITIONAL', 20);
-define('PARTICLE', 21);
-define('PREPOSITION', 22);
-define('INJECTIVE', 23);
-define('HEBREW', 23);
-define('ARAMAIC', 23);
-
-/// Number
-define('SINGULAR', 24);
-define('PLURAL', 25);
-
-/// Person
-define('FIRST_PERSON', 26);
-define('SECOND_PERSON', 27);
-define('THIRD_PERSON', 28);
-
-/// Tense
-define('PRESENT', 29);
-define('IMPERFECT', 30);
-define('FUTURE', 31);
-define('AORIST', 32);
-define('PERFECT', 33);
-define('PLUPERFECT', 34);
-define('NO_TENSE', 35); ///NOTE: Adverbial imperative?
-
-/// Voice
-define('ACTIVE', 36);
-define('MIDDLE', 37);
-define('PASSIVE', 38);
-define('DEPONENT', 39); ///TODO: Split deponant out.
-define('EITHER_MIDDLE_OR_PASSIVE', 40); ///TODO: Determine if these can be reconciled?
-define('NO_VOICE', 41);
-
-/// Mood
-define('INDICATIVE', 42);
-define('SUBJUNCTIVE', 43);
-define('IMPERATIVE', 44);
-define('INFINITIVE', 45);
-define('OPTATIVE', 46);
-define('PARTICIPLE', 47);
-define('IMPERATIVE_PARTICIPLE', 48); ///TODO: Determine if there should be two participles.
-
-/// Gender
-define('MASCULINE', 49);
-define('FEMININE', 50);
-define('NEUTER', 51);
-
-/// Case (currently only 5-case system)
-define('NOMINATIVE', 52);
-define('GENITIVE', 53);
-define('ACCUSATIVE', 54);
-define('DATIVE', 55);
-define('VOCATIVE', 56);
-
-/// Degree
-define('COMPARATIVE', 57);
-define('SUPERLATIVE', 58);
-
-/// Miscellaneous
-define('INTERROGATIVE', 59);
-define('NEGATIVE', 60);
-define('PARTICLE', 61);
-define('MIDDLE_SIGNIFICANCE', 62);
-
-/// Declinability
-define('DECLINABLE', 63);
-define('INDECLINABLE', 64);
-
-/// Numerical (adjective type?)
-define('NUMERICAL', 65);
-
-/// Noun type
-define('NORMAL_NOUN', 66); /// Should this just be zero.
-define('PROPER_NOUN', 67);
-define('LETTER', 68);
-define('OTHER_NOUN', 69);
-
-/// Form
-define('ABBRIVIATED', 70);
-define('CONTRACTED', 71);
-define('APOCOPATED', 72);
-define('IRREGULAR', 73);
-
-/// Dialect
-define('ATTIC', 74);
-define('AEOLIC', 75);
-
-/// Transitivity
-define('TRANSITIVE', 76);
-define('INTRANSITIVE', 77); ///TODO: Need to add this in.  Determine if there is bitransitive.
+///TODO: Figue out the best way to handle this.  Currently, there is a short list of constants
+///      that is used to ensure that valid data is sent.  The downside is that an array must be
+///      sent containing the general grammatical type plus the specific value.  It also requires
+///      a long switch construct.
+define('PART_OF_SPEECH', 1);
+define('NUMBER', 2);
+define('PERSON', 3);
+define('TENSE', 4);
+define('VOICE', 5);
+define('MOOD', 6);
+define('GENDER', 7);
+define('CASE_5', 8);
+define('DEGREE', 9);
+define('DECLINABILITY', 10);
+define('NUMERICAL', 11);
+define('NOUN_TYPE', 12);
+define('FORM', 13);
+define('DIALECT', 14);
+define('TRANSITIVITY', 15);
+define('SECOND_FORM', 16); ///TODO: See if second_form can or should be combined with FORM.
+define('MISCELLANEOUS', 17);
+define('ADDED', 18);
+define('DIVINE', 19);
+define('RED', 20);
 
 
 function interpret_json($json, $sphinx)
 {
-	/// $json ex: '["love",[NOUN]]' OR '["go",[IMPERATIVE,SINGULAR],[1,1]]'
+	/// $json '["WORD",[[GRAMMAR_TYPE,VALUE],[...]],[INCLUDE]]'
+	/// $json ex1: '["love",[[PART_OF_SPEECH,1]],[1]]' == love AS NOUN
+	/// $json ex2: '["go",[[MOOD,3],[NUMBER,1]],[1,0]]' == go AS IMPERATIVE, NOT SINGULAR
+	
 	$query_array = json_decode($json);
-	foreach ($query_array[1] as $key => $morphology) {
-		switch ($morphology) {
-			case NOUN:
-				$sphinx->SetFilter('part_of_speech', array(1), $query_array[2][$key]);
+	foreach ($query_array[1] as $key => $morphology_arr) {
+		switch ($morphology_arr[0]) {
+			case ADDED:
+				$attr = 'added';
 				break;
-			case VERB:
-				$sphinx->SetFilter('part_of_speech', array(2), $query_array[2][$key]);
+			case divine:
+				$attr = 'divine';
 				break;
+			case red:
+				$attr = 'quotation'; ///TODO: Decide on a consistent name for this.
+				break;
+			case PART_OF_SPEECH:
+				$attr = 'part_of_speech';
+				break;
+			case NUMBER:
+				$attr = 'number';
+				break;
+			case PERSON:
+				$attr = 'person';
+				break;
+			case TENSE:
+				$attr = 'tense';
+				break;
+			case VOICE:
+				$attr = 'voice';
+				break;
+			case MOOD:
+				$attr = 'mood';
+				break;
+			case GENDER:
+				$attr = 'gender';
+				break;
+			case CASE_5:
+				$attr = 'case_5';
+				break;
+			case DEGREE:
+				$attr = 'degree';
+				break;
+			case DECLINABILITY:
+				$attr = 'indeclinable'; ///FIXME: This should probably be changed to 'declinability' and the values reversed.
+				break;
+			case NUMERICAL:
+				$attr = 'numerical';
+				break;
+			case NOUN_TYPE:
+				$attr = 'noun_type';
+				break;
+			case FORM:
+				$attr = 'form';
+				break;
+			case DIALECT:
+				$attr = 'dialect';
+				break;
+			case TRANSITIVITY:
+				$attr = 'transitive'; ///FIXME: This should probalby be changed to match the constant.
+				break;
+			case MISCELLANEOUS:
+				$attr = 'extra'; ///FIXME: This should probalby be changed to match the constant.
+				break;
+			case SECOND_FORM:
+				$attr = 'second_form';
+				break;
+			default:
+				///TODO: Determine if an error should be thrown.
+				/// Skip the invalid grammatical form.
+				continue 2;
 		}
+		$sphinx->SetFilter($attr, array((int)$morphology_arr[1]), (bool)$query_array[2][$key]);
 	}
 }
