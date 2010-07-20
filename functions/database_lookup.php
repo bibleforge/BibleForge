abfd2fe55016d196cfefad4e7efd5185c9bc98f3
BibleForge <info@bibleforge.com>
2010-07-20 08:22:57 -0500

    Fixes a bug with looking up previous results.
    Note to self, don't use ++/-- intermixed with other code because
    you might delete it accidentally when cleaning up code.

--- functions/database_lookup.php
+++ functions/database_lookup.php
@@ -57,6 +57,14 @@ function retrieve_verses($verse_id, $direction, $limit, $in_paragraphs = true)
     }
     
     $SQL_query  = 'SELECT id, words' . $extra_fields . ' FROM ' . BIBLE_VERSES . ' WHERE id ' . $operator . (int)$verse_id . $order_by . ' LIMIT ' . $limit;
+    
+    ///NOTE: Unbuffered queries start returning data as soon as the first row is available;
+    ///      however, when the PHP script ends, if all of the data has not been fetched,
+    ///      PHP will fetch all of the rest of the verses.  Therefore, the PHP script does not
+    ///      end any sooner than with unbuffered queries, but is might be able to start sending data
+    ///      back to the client sooner.
+    ///TODO: Determine the best way to query the database: consider mysql_query() vs mysql_unbuffered_query(),
+    ///      as well as mysqli_store_result() plus mysql_use_result().
     $SQL_res    = mysql_unbuffered_query($SQL_query) or die('SQL Error: ' . mysql_error() . '<br>' . $SQL_query);
     
     
@@ -65,22 +73,40 @@ function retrieve_verses($verse_id, $direction, $limit, $in_paragraphs = true)
         $verse_numbers = array();
         $paragraphs    = array();
         
-        $verse_count    = 0;
+        $verse_count = 0;
+        $break_after = false; 
         
         while ($row = mysql_fetch_assoc($SQL_res)) {
             if ($row['paragraph']) {
                 /// Did it find enough verses to send to the browser.
                 if ($verse_count > 35) {
-                    break;
+                    /// The first verse should be at a paragraph beginning, and the last verse
+                    /// should be just before one. Therefore, when looking up previous verses,
+                    /// we must get this verse (because previous lookups are in reverse).
+                    /// So, additional lookups should stop now because the next verse is at the
+                    /// beginning of a paragraph, but previous lookups need to get this last verse,
+                    /// which is actually the first verse (because the arrays will be reversed shortly).
+                    if ($direction == PREVIOUS) {
+                        $break_after = true;
+                    } else {
+                        break;
+                    }
                 }
             }
             
             $verse_HTML[]    = $row['words'];
             $verse_numbers[] = $row['id'];
             $paragraphs[]    = $row['paragraph'];
+            
+            if ($break_after) {
+                break;
+            }
+            ++$verse_count;
         }
         
         if ($direction == PREVIOUS) {
+            /// When looking up previous verses, the results are returned by the database in reverse,
+            /// so they must be reversed (put in correct order) before being sent to the client.
             krsort($verse_HTML);
             krsort($verse_numbers);
             krsort($paragraphs);
