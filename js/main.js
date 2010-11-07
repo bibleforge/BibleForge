21ed04c6a7951992532de547dcbb158e7a8cda77
BibleForge <info@bibleforge.com>
2010-11-07 15:51:56 -0600

    Added a hack (at least temporarily) to make the wrench menu appear on IE8-.
    IE8- will not return a function that is eval'ed, so this makes it return a
    JSON-like array with the function inside.  However, IE8- cannot seem to run
    the rest of secondary.js, so dropping IE8 support is likely.

--- js/main.js
+++ js/main.js
@@ -102,6 +102,14 @@ BF.include = (function ()
                         /// Load and run the new code.
                         ///NOTE: IE8- does not eval the code correctly.
                         include_func = evaler(ajax.responseText);
+                        ///NOTE: IE8 will not return a function by itself.
+                        /*@cc_on
+                            @if (@_jscript_version < 9)
+                                /// Wrap the code in an array and slice off the ending semicolon so that it is essentially a JSON array.
+                                include_func = evaler("[" + ajax.responseText.slice(0, ajax.responseText.trim().length - 1) + "]")[0];
+                            @end
+                        @*/
+
                         if (typeof include_func == "function") {
                             include_func(context);
                         }
