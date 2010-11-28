4b82762fd142c3042627e2a01776033eb3e9670a
BibleForge <info@bibleforge.com>
2010-11-28 09:37:30 -0600

    Added alt attribute to magnifying glass and wrench images.
    NOTE: The HTML texts need to be configurable via a language specific file.

--- js/secondary.js
+++ js/secondary.js
@@ -450,6 +450,8 @@
         ///TODO: Determine where this gif data should be.
         wrench_button.src   = "data:image/gif;base64,R0lGODdhEAAQAMIIAAEDADAyL05OSWlpYYyLg7GwqNjVyP/97iwAAAAAEAAQAAADQ3i6OwBhsGnCe2Qy+4LRS3EBn5JNxCgchgBo6ThwFDc+61LdY6m4vEeBAbwMBBHfoYgBLW8njUPmPNwk1SkAW31yqwkAOw==";
         wrench_button.title = BF.lang.wrench_title;
+        ///TODO: Determine if the alt tag should be shorter, like "Configure."
+        wrench_button.alt   = BF.lang.wrench_title;
         
         wrench_label.htmlFor = wrench_button.id;
         
