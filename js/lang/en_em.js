6f2878af9d4f50caae0c3cfbac2f32b500d4eb42
BibleForge <info@bibleforge.com>
2012-02-08 02:16:15 +0800

    Updated JSLint properties.

--- js/lang/en_em.js
+++ js/lang/en_em.js
@@ -38,13 +38,13 @@
     books_long_pretitle, books_short, chapter, chapter_count, configure, 
     determine_reference, done, eli, en_em, enci, entli, exec, found_plural, 
     found_singular, ful, full_name, fulness, grammar_keywords, grammar_marker, 
-    grammar_marker_len, grammar_separator, help, ical, icate, iciti, 
-    identifier, indexOf, italics_explanation, iveness, iviti, ization, izer, 
-    lang_id, langs, length, linked_to_orig, loaded, logi, ness, ousli, ousness, 
-    paragraphs, prepare_highlighter, prepare_query, prepare_search, psalm, 
-    query_button_alt, query_button_title, query_explanation, red_letters, 
-    replace, self, short_name, slice, split, subscription, test, tional, title, 
-    toLowerCase, view, wrench_title
+    grammar_marker_len, grammar_separator, help, ical, icate, iciti, id, 
+    indexOf, italics_explanation, iveness, iviti, ization, izer, lang_id, 
+    langs, length, linked_to_orig, loaded, logi, ness, no_results1, 
+    no_results2, ousli, ousness, paragraphs, prepare_highlighter, 
+    prepare_query, prepare_search, psalm, query_button_alt, query_button_title, 
+    query_explanation, red_letters, replace, self, short_name, slice, split, 
+    subscription, test, tional, title, toLowerCase, view, wrench_title
 */
 
 /**
