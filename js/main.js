5c95b40df900a6449de33686a9d0b09d99174617
BibleForge <info@bibleforge.com>
2012-05-09 19:06:36 +0800

    Tweaked CSS to align the drop down arrow and context menu better.

--- js/main.js
+++ js/main.js
@@ -3341,7 +3341,7 @@
             ///TODO: Determine if there is any problem hitting the server again so quickly.
             window.setTimeout(function ()
             {
-                BF.include("/js/secondary.js?10177106", {
+                BF.include("/js/secondary.js?10191151", {
                     content_manager: content_manager,
                     langEl:          langEl,
                     page:            page,
