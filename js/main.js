0018128445d92f45668cbf919ca1bd543aec04ce
BibleForge <info@bibleforge.com>
2012-05-17 16:35:00 +0800

    Fixed issue with a Chrome bug that does not handle table-row correctly.
    Applied fix from c915d661a99a6b06517fac486416d8d2497471ec.

--- js/main.js
+++ js/main.js
@@ -3375,7 +3375,7 @@
             ///TODO: Determine if there is any problem hitting the server again so quickly.
             window.setTimeout(function ()
             {
-                BF.include("/js/secondary.js?10654328", {
+                BF.include("/js/secondary.js?10882374", {
                     content_manager: content_manager,
                     langEl:          langEl,
                     page:            page,
