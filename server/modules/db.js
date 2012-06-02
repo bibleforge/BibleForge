a32463c83704f62eaf506e73f3fb6721b9a265c6
BibleForge <info@bibleforge.com>
2012-06-02 17:45:29 +0800

    Queue database queries until it successfully connects to the database server.

--- server/modules/db.js
+++ server/modules/db.js
@@ -10,21 +10,69 @@
 
 "use strict";
 
+/**
+ *
+ * @todo Reconnect to the server if it gets disconnected.
+ */
 this.db = function (config)
 {
     return (function ()
     {
-        var db = new (require("db-mysql")).Database({
+        var connected,
+            db = new (require("db-mysql")).Database({
             hostname: config.host,
             user:     config.user,
             password: config.pass,
-            database: config.base,
-            async: false
-        });
+            database: config.base
+        }),
+            queue = (function ()
+            {
+                var queries = [];
+                
+                return {
+                    /**
+                     *
+                     * @todo Remove old queued queries when the client that requested them closes.
+                     */
+                    add: function (sql, callback)
+                    {
+                        queries[queries.length] = {sql: sql, callback: callback};
+                    },
+                    flush: function ()
+                    {
+                        var query;
+                        
+                        if (queries.length) {
+                            
+                            query = queries.shift();
+                            
+                            db.query().execute(query.sql, [], function (err, data)
+                            {
+                                if (typeof query.callback === "function") {
+                                    query.callback(data, err);
+                                }
+                                queue.flush();
+                            });
+                        }
+                    }
+                };
+            }());
         
-        db.connect({async: false});
-            
-        db.query().execute("SET NAMES 'utf8'", {async: false});
+        function connect()
+        {
+            db.connect(function (err)
+            {
+                if (err) {
+                    setTimeout(connect, 50);
+                } else {
+                    db.query().execute("SET NAMES 'utf8'", {async: false});
+                    connected = true;
+                    queue.flush();
+                }
+            });
+        }
+        
+        connect();
         
         return {
             escape: function (str)
@@ -37,12 +85,16 @@ this.db = function (config)
             },
             query: function (sql, callback)
             {
-                db.query().execute(sql, [], function (err, data)
-                {
-                    if (typeof callback === "function") {
-                        callback(data, err);
-                    }
-                });
+                if (connected) {
+                    db.query().execute(sql, [], function (err, data)
+                    {
+                        if (typeof callback === "function") {
+                            callback(data, err);
+                        }
+                    });
+                } else {
+                    queue.add(sql, callback);
+                }
             }
         };
     }());
