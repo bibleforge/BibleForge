6061f3dfcb30707e8c298f9b675555dfd830fd30
BibleForge <info@bibleforge.com>
2012-07-26 11:54:23 +0800

    Removed checking for em dashes from en_em.js.

--- client/js/main.js
+++ client/js/main.js
@@ -49,7 +49,7 @@
     if (!BF.lang.en_em) {
         BF.langs.en_em = {
             full_name: "Early Modern English (1611)",
-            modified:  16880413
+            modified:  16905419
         };
     }
     
