0c52365db24172b4c3f48acf5ac344f1d10717f7
BibleForge <info@bibleforge.com>
2012-12-01 17:51:38 +0800

    Some Night Mode CSS improvements.

--- client/js/main.js
+++ client/js/main.js
@@ -1053,7 +1053,7 @@
                         if (values.new_val) {
                             if (!css_added) {
                                 link_tag = document.createElement("link");
-                                link_tag.href = "/styles/night.css?27994453";
+                                link_tag.href = "/styles/night.css?27994476";
                                 link_tag.rel  = "stylesheet";
                                 document.getElementsByTagName("head")[0].appendChild(link_tag);
                                 css_added = true;
