872d2dee996cabcc5c7c14336e8e6c25353cff2c
BibleForge <info@bibleforge.com>
2012-12-02 14:45:17 +0800

    Use a better background selection color for WebKit in Night Mode.

--- client/js/main.js
+++ client/js/main.js
@@ -1053,7 +1053,7 @@
                         if (values.new_val) {
                             if (!css_added) {
                                 link_tag = document.createElement("link");
-                                link_tag.href = "/styles/night.css?28016323";
+                                link_tag.href = "/styles/night.css?28069658";
                                 link_tag.rel  = "stylesheet";
                                 ///TODO: Add a wait cursor
                                 document.getElementsByTagName("head")[0].appendChild(link_tag);
