705c3d43d1127f926ea649f48391395c810f6f23
BibleForge <info@bibleforge.com>
2013-03-20 01:55:55 +0800

    Added the missing short form of John (約) in Traditional Chinese.

--- client/js/main.js
+++ client/js/main.js
@@ -58,13 +58,13 @@ document.addEventListener("DOMContentLoaded", function ()
     if (!BF.lang.zh_s) {
         BF.langs.zh_s = {
             full_name: "简体中文 (CKJV)",
-            modified: 37354055,
+            modified: 37354667,
         };
     }
     if (!BF.lang.zh_t) {
         BF.langs.zh_t = {
             full_name: "繁體中文 (CKJV)",
-            modified: 37353996,
+            modified: 37354680,
         };
     }
     
