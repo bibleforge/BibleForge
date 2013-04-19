2459afc989a8fae3121fcd00f20631544ebbde25
BibleForge <info@bibleforge.com>
2013-04-19 10:56:57 +0800

    Convert Chinese commas to ASCII commas for convince.

--- client/js/main.js
+++ client/js/main.js
@@ -67,14 +67,14 @@ document.addEventListener("DOMContentLoaded", function ()
     if (!BF.lang.zh_s) {
         BF.langs.zh_s = {
             full_name: "简体中文 (CKJV)",
-            modified: 39979112,
+            modified: 39979182,
             match_lang: /zh-c(?:n|hs)/i,
         };
     }
     if (!BF.lang.zh_t) {
         BF.langs.zh_t = {
             full_name: "繁體中文 (CKJV)",
-            modified: 39941067,
+            modified: 39979184,
             match_lang: /zh(?:-c(?!n|hs))?/i,
         };
     }
