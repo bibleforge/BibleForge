b5dd39bea560ade64d80ca012dc2893d85bd34ec
BibleForge <info@bibleforge.com>
2013-04-25 00:40:50 +0800

    Updated CKJV.

--- client/js/main.js
+++ client/js/main.js
@@ -67,14 +67,14 @@ document.addEventListener("DOMContentLoaded", function ()
     if (!BF.lang.zh_s) {
         BF.langs.zh_s = {
             full_name: "简体中文 (CKJV)",
-            modified: 40207346,
+            modified: 40402258,
             match_lang: /zh-c(?:n|hs)/i,
         };
     }
     if (!BF.lang.zh_t) {
         BF.langs.zh_t = {
             full_name: "繁體中文 (CKJV)",
-            modified: 40203754,
+            modified: 40397891,
             match_lang: /zh(?:-c(?!n|hs))?/i,
         };
     }
